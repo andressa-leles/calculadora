a10e673d7b7eb966f3d0f3c521c88a1b0946b374
Andressa Leles <leles.andressa@gmail.com>
2025-07-24 19:59:55 -0300

    Calculadora JS

---
+++ script.js
@@ -0,0 +1,48 @@
+function calcular(){
+    const valor1 = parseInt(document.getElementById("valor1").value)
+    const valor2 = parseInt(document.getElementById("valor2").value)
+
+   let resultado = ""
+
+   function soma(valor1, valor2){
+        return valor1 + valor2
+
+   }
+
+   function subtracao(valor1, valor2){
+        return valor1 - valor2
+
+   }
+
+   function multiplicacao(valor1, valor2){
+        return valor1 * valor2
+   }
+
+   function divisao(valor1, valor2){
+        return valor1 / valor2
+   }
+
+   const operacao = document.getElementById("operacao").value
+
+    switch (operacao) {
+        case "soma":
+            resultado = soma(valor1, valor2)
+            break;
+        case "subtracao":
+            resultado = subtracao(valor1, valor2)
+            break;
+        case "multiplicacao":
+            resultado = multiplicacao(valor1, valor2) 
+            break; 
+        case "divisao":
+            resultado = divisao(valor1,valor2)   
+            break;  
+        default:
+            resultado = "Operação inválida"
+            break;
+    }
+
+    document.getElementById('resultado').innerText = resultado
+}
+
+document.getElementById("btnCalcular").addEventListener("click", calcular);
